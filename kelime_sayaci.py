ab280db55af0efcd65e5d003f3598f257512a9c2
furkan-atilla <furkan.atilla@outlook.com.tr>
2023-03-17 22:45:55 +0300

    init

---
+++ kelime_sayaci.py
@@ -0,0 +1,29 @@
+def kelime_say(metin):
+    kucuk_metin = metin.lower()
+    kelime_listesi = metin.split()
+    kelime_sayilari = {}
+    
+    for kelime in kelime_listesi:
+        if kelime in kelime_sayilari:
+            kelime_sayilari[kelime] += 1
+        else:
+            kelime_sayilari[kelime]= 1
+        
+    return kelime_sayilari    
+    
+metin = input("Bir metin giriniz: ")
+sonuc = kelime_say(metin)
+
+print(sonuc)
+        
+        
+        
+  
+
+
+
+    
+
+
+
+
